c997146672df472d384d9ef62558df6d0d2da210
AntSpanish <114097276+AntSpanish@users.noreply.github.com>
2023-01-19 12:26:30 -0500

    my information v1
    First assignment of CS175L-01.

---
+++ myInfo.py
@@ -0,0 +1,11 @@
+'''
+CS175L-01
+Ant Spanish
+myInfo
+'''
+#CS175L-01
+#Ant Spanish
+#myInfo
+print("Name: Ant Spanish")
+print("Location: Union Beach, NJ, 07735")
+print("Phone Number: 7326706130")
