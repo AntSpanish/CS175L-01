db2c5237a52f3a55f9d8499101aa27278b7e1c9a
AntSpanish <114097276+AntSpanish@users.noreply.github.com>
2023-02-11 22:27:23 -0500

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -6,28 +6,69 @@
 vegetarian=False
 vegan=False
 glutenFree=False
+retry=False
+loop=True
 
-#Input
-response=str(input('Is anyone in your party vegetarian? '))
-if response.lower()=='yes':
-    vegetarian=True
+#Input2.0
+while loop:
+    response=str(input('\nIs anyone in your party vegetarian? '))
+    if not(response.lower()=='yes' or response.lower()=='no'):
+        retry=True
+    while (retry==True):
+        response=input('Please enter \'yes\' or \'no\': ')
+        if not(response.lower()=='yes' or response.lower()=='no'):
+            retry=True
+        else:
+            retry=False
+    if (response.lower()=='yes'):
+        vegetarian=True
 
-response=str(input('Is anyone in your party vegan? '))
-if response.lower()=='yes':
-    vegan=True
+    response=str(input('Is anyone in your party vegan? '))
+    if not(response.lower()=='yes' or response.lower()=='no'):
+        retry=True
+    while (retry==True):
+        response=input('Please enter \'yes\' or \'no\': ')
+        if not(response.lower()=='yes' or response.lower()=='no'):
+            retry=True
+        else:
+            retry=False
+    if (response.lower()=='yes'):
+        vegan=True
 
-response=str(input('Is anyone in your party gluten-free? '))
-if response.lower()=='yes':
-    glutenFree=True
+    response=str(input('Is anyone in your party gluten-free? '))
+    if not(response.lower()=='yes' or response.lower()=='no'):
+        retry=True
+    while (retry==True):
+        response=input('Please enter \'yes\' or \'no\': ')
+        if not(response.lower()=='yes' or response.lower()=='no'):
+            retry=True
+        else:
+            retry=False
+    if (response.lower()=='yes'):
+        glutenFree=True
 
 #Output
+    print('\nHere are your restaurant choices.')
+    if not vegetarian and not vegan and not glutenFree:
+        print('Joe\'s Gourment Burgers')
+    if not vegan:
+        print('Main Street Pizza Company')
+    print('Corner Café')
+    if not vegan and not glutenFree:
+        print('Mama\'s Fine Italian')
+    print('The Chef\'s Kitchen')
 
-print('\nHere are your restaurant choices.')
-if not vegetarian and not vegan and not glutenFree:
-    print('Joe\'s Gourment Burgers')
-if not vegan:
-    print('Main Street Pizza Company')
-print('Corner Café')
-if not vegan and not glutenFree:
-    print('Mama\'s Fine Italian')
-print('The Chef\'s Kitchen')
+#Loop Statement
+    yesOrNo=str(input('\nEnter \'yes\' if you would like to do another restaurant search (enter \'no\' to end): '))
+    if not (yesOrNo.lower()=='yes' or yesOrNo.lower()=='no'):
+        retry=True
+    while (retry==True):
+        yesOrNo=str(input('Please enter \'yes\' or \'no\': '))
+        if not (yesOrNo.lower()=='yes' or yesOrNo.lower()=='no'):
+            retry=True
+        else:
+            retry=False
+    if (yesOrNo.lower()=='yes'):
+        loop=True
+    elif (yesOrNo.lower()=='no'):
+        loop=False
