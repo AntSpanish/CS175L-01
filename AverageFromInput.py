72af567aa232d3e726194ff4c75da5f622890ea5
AntSpanish <114097276+AntSpanish@users.noreply.github.com>
2023-03-09 23:47:34 -0500

    V2 With Exception Handling

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -2,23 +2,48 @@
 #Ant Spanish
 #AverageFromInput
 
-def main():
-    num_file = open("numbers.txt", 'r')
+def read_file_loop(num_file):
+    total = 0
+    count = 0
 
-    total=0
-    count=0
-    
     for x in num_file:
-        count+=1
-        num=int(x)
-        total+=num
-        print(f'I read in {count} number(s)', f'\tCurrent number is: {num:.2f}', f'\tTotal is: {total:.2f}')
+        try:
+            num = float(x)
+        except ValueError:
+            x = x.strip('\n')
+            print('Non-numeric data found in file:',x)
+        else:
+            count+=1
+            total+=num
+            print(f'I read in {count} number(s)',f'\tCurrent number is: {num:.2f}', f'\tTotal is: {total:.2f}')
 
-    avg=total/count
+    return count,total
+            
+def average_from_values(total,count):
+    if count==0:
+        print('No numeric values to take average of.')
+        return False
+    else:
+        avg = total/count
+        return avg
 
-    print(f'Average: {avg:.1f}')
+def print_average(avg):
+    print('Average (from numeric values):',f'{avg:.1f}')
+    
+def main():
+    try:
+        num_file = open("numbers.txt", 'r')
 
-    num_file.close()
+        count,total = read_file_loop(num_file)
 
+        avg = average_from_values(total,count)
+
+        if not avg==False:
+            print_average(avg)
+
+        num_file.close()
+    except IOError:
+        print('Error occurred when reading file.')
+        
 if __name__ == '__main__':
     main()
