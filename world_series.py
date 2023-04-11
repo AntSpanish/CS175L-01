664f2ad3c6ddc30702a0788b193c59b9f3ba8c5d
AntSpanish <114097276+AntSpanish@users.noreply.github.com>
2023-04-10 22:04:58 -0400

    Add files via upload

--- world_series.py
+++ world_series.py
@@ -8,20 +8,31 @@ def world_series_winners_list(file):
         line = line.rstrip('\n')
         world_series_winners.append(line)
     return world_series_winners 
-
+    
 def main():
     running=True
     file=open(r'C:\Users\17326\OneDrive\Documents\Python Files\WorldSeriesWinners.txt')
     world_series=world_series_winners_list(file)
-    print(world_series)
     while running:
         team_name=input('\nEnter the name of the team (or q to quit). ').lower()
         if team_name=='q':
             print('Goodbye.')
             running=False
         else:
+            appear_once=[]
             team_in_world_series=[line for line in world_series if team_name in line.lower()]
-            print(f'The {team_name} won the world series {len(team_in_world_series)} times between 1903 and 2009.')
+            team_in_world_series.sort()
+            for line in team_in_world_series:
+                if line not in appear_once:
+                    appear_once.append(line)
+            print(f'\nTeam list:')
+            for line in appear_once:
+                print(f'{line:>27}')
+            print('')
+            choice_specific=input('Choose a specific team. ')
+            specific_team=[line for line in team_in_world_series if line.lower()==choice_specific.lower()]
+            
+            print(f'The {choice_specific} won the world series {len(specific_team)} times between 1903 and 2009.')
     
 if __name__ == '__main__':
     main()
